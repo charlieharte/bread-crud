531f25dd5edce614acb7bcc72a7bd265da1e2675
Charlie Harte <charlieharte@Charlies-MBP.attlocal.net>
2023-05-04 18:48:10 -0400

    delete bread

--- controllers/bread.js
+++ controllers/bread.js
@@ -17,7 +17,8 @@ router.get('/new', (req,res) => {
 router.get('/:index', (req, res) => {
   const { index } = req.params
   res.render('show', {
-    bread: Bread[index]
+    bread: Bread[index],
+    index: index
   })
 })
 
@@ -32,6 +33,12 @@ router.post('/', (req,res) => {
 
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+router.delete('/:index', (req,res) => {
+  const { index } = req.params
+  Bread.splice(index,1)
+  res.status(303).redirect('/breads')
 })
 
 module.exports = router
